94689aa233b775a0aba868b4a1a4b4ed655786fe
Quyen <146425082+Grozay@users.noreply.github.com>
2024-02-17 16:48:09 +0700

    login admin

---
+++ admin/loginAdmin.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+if (isset($_GET['error'])) {
+    $error = 'Password or login name is wrong';
+}
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION["username"];
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Creative Colorlib SignUp Form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script type="application/x-javascript">
+        addEventListener("load", function() {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <!-- <link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet"> -->
+    <link href="./loginAdmin.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="main-w3layouts wrapper">
+        <h1>Sign Up </h1>
+        <div class="main-agileinfo">
+            <div class="agileits-top">
+                <form action="process-loginadmin.php" method="post">
+                    <input class="text" type="text" name="username" placeholder="Username" required="" value="<?= (isset($username)) ? $username : '' ?>">
+                    <input class="text" type="password" name="password" placeholder="Password" required="">
+                    <div>
+                        <span class="error-login"><?= (isset($error)) ? $error : '' ?></span>
+                    </div>
+                    <input type="submit" value="LOG IN">
+                </form>
+            </div>
+        </div>
+        <!-- <ul class="colorlib-bubbles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul> -->
+    </div>
+    <!-- //main -->
+</body>
+
+</html>
---
+++ admin/process-loginadmin.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (isset($_POST) && !empty($_POST)) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    echo $username . $password;
+
+    $conn = mysqli_connect('localhost', 'root', '', 'projectmini');
+    if (!$conn) {
+        die("Connection Failed!");
+    }
+
+    $query = "select * from admin where username = '$username' and password = '$password'";
+    $result = mysqli_query($conn, $query);
+    $adminaccout = mysqli_fetch_array($result);
+    // var_dump($useraAccout);
+    mysqli_close($conn);
+    if ($adminaccout == null) {
+        header('location: loginAdmin.php?error=loginFail');
+        $_SESSION["username"] = $adminaccout['username'];
+    } else {
+        $_SESSION["id"] = $adminaccout['id'];
+        header("location: productlist.php");
+    }
+}
